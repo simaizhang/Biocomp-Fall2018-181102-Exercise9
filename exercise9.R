f0f6a4088d69f7a338c3ea10947d8dc8d4fe5735
Simai Zhang <43488401+simaizhang@users.noreply.github.com>
2018-11-08 21:57:09 -0500

    final

--- exercise9.R
+++ exercise9.R
@@ -5,9 +5,10 @@ setwd("~/bio")
 LDH=read.table("LDH.txt", header = T, stringsAsFactors = F)
 library(ggplot2)
 a=ggplot(data=LDH,aes(x=pyruvate,y=activity))
-a+ggtitle("Michaelis-Menton Plot for LDH")+geom_point()+stat_smooth(method="auto")+theme_bw()+theme(panel.grid = element_blank())+
-  xlab("[pyruvate] (mM)") +  ylab("Activity (delta A340/min)") 
-# ?? I try to put the title in the center, I tried "+theme(plot.title=element_text(hjust = 0.5))", but it does not work
+a+ggtitle("Michaelis-Menton Plot for LDH")+geom_point()+stat_smooth(method="auto")+theme_bw()+
+  theme(panel.grid = element_blank(), plot.title=element_text(hjust = 0.5))+
+  xlab("[pyruvate] (mM)") +  ylab("Activity (delta A340/min)")
+
 
 # 2.1
 setwd("~/bio/Biocomp-Fall2018-181102-Exercise9/")
@@ -20,4 +21,7 @@ b+geom_bar(aes(x=as.factor(region),y=observations), stat = "summary",
 c=ggplot(data=data,aes(x=region,y=observations))
 c+geom_jitter()+geom_point()+coord_cartesian()+theme_bw()
 
+# 2.3
+# From the bar plot???we can not tell the difference between 4 regions, since they all have similar mean value.
+# However, from the scatter plots, we can clearly see the different distributions for 4 regions.
            
